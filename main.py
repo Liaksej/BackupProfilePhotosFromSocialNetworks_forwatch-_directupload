769932de69bb846115f417079f209af67ee51c9b
Liaksej <tarusov.liaksej@gmail.com>
2022-07-01 21:11:54 +0200

    Make a new filename 1.0
    In this version the program is capable to save name like a counting of likes.

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
 import requests
 import os
+from pprint import pprint
 
 
 class VkToYaDsk:
+
     def __init__(self, token: str):
         self.token = token
 
-    def download_photo(self, file_path_to_save: str, owner_id: int):
+    def download_photo(self, file_path_to_save, owner_id: int):
         url = 'https://api.vk.com/method/photos.get'
         # headers = self.get_headers()
         params = {'owner_id': owner_id, 'album_id': 'profile', 'extended': 1, 'access_token': self.token, 'v': '5.131'}
         response = requests.get(url=url, params=params)
-        photoalbums = response.json()
-        album_best_resolution = []
-        for y in photoalbums['response']['items']:
-            maximum_height_photo = y['sizes'][-1]
-            album_best_resolution.append(maximum_height_photo['url'])
-        return album_best_resolution
-
-    def save_photo(self, file_path_to_save, owner_id):
+        photoalbum = response.json()
         if not os.path.exists(file_path_to_save):
             os.mkdir(file_path_to_save)
             print('Successfully created directory', file_path_to_save)
-        listik = self.download_photo(file_path_to_save, owner_id)
-        for x, y in enumerate(listik):
-            reqs = requests.get(y)
-            with open(file_path_to_save + '/' + f'{str(x)}.jpg', 'wb') as code:
+        album_best_resolution = []
+        data_for_json = []
+        content_for_json_item = {}
+        for y in photoalbum['response']['items']:
+            maximum_height_photo = y['sizes'][-1]
+            album_best_resolution.append(maximum_height_photo['url'])
+            content_for_json_item['file_name'] = (f'{y["likes"]["count"]}.jpg')
+            content_for_json_item['size'] = (f'{y["sizes"][-1]["height"]}x{y["sizes"][-1]["width"]}.jpg')
+            data_for_json.append(content_for_json_item)
+            reqs = requests.get(y['sizes'][-1]['url'])
+            with open(file_path_to_save + '/' + f'{y["likes"]["count"]}.jpg', 'wb') as code:
                 code.write(reqs.content)
+        with open(file_path_to_save + '/' + f'photo_data.json', 'w') as file:
+            file.write(f'{data_for_json}')
+
+        # counter_of_words_in_phrase = {}
+        # for element in data_for_json:
+        #     m = element['file_name']
+        #     if m in counter_of_words_in_phrase:
+        #         counter_of_words_in_phrase[m] += 1
+        #     else:
+        #         counter_of_words_in_phrase[m] = 1
+        # for x counter_of_words_in_phrase.values():
+        #     if x > 1:
+        #         del counter_of_words_in_phrase[x]
+        # for x in data_for_json:
+        #     if x['file_name'] in counter_of_words_in_phrase.keys():
+        #     x['file_name'] = (f'{y["likes"]["count"]} {}.jpg') + return album_best_resolution, data_for_json
+        # pprint(photoalbum)
+
+    # def save_photo(self, file_path_to_save):
+    #     if not os.path.exists(file_path_to_save):
+    #         os.mkdir(file_path_to_save)
+    #         print('Successfully created directory', file_path_to_save)
+    #     listik = self.download_photo(owner_id)
+    #     for y in enumerate(listik[0]):
+    #         reqs = requests.get(y)
+    #         with open(file_path_to_save + '/' + f'{filestr(x)}.jpg', 'wb') as code:
+    #             code.write(reqs.content)
         # return 'Done'
 
 
@@ -62,10 +91,11 @@ if __name__ == '__main__':
     owner_id = 7451160
     file_path_to_save = 'Photos'
     downloader = VkToYaDsk(token_vk)
-    downloader.save_photo(file_path_to_save, owner_id)
+    # downloader.save_photo(file_path_to_save)
+    downloader.download_photo(file_path_to_save, owner_id)
 
-    token_yd = 'AQAAAAAirg3lAADLWyKCRKHJ4kpflbLSVeaV5Go'
-    filelist = os.listdir('Photos')
-    disk_file_path = 'Photos_VK'
-    uploader = YaUploader(token_yd)
-    result = uploader.upload(disk_file_path, filelist)
+    # token_yd = 'AQAAAAAirg3lAADLWyKCRKHJ4kpflbLSVeaV5Go'
+    # filelist = os.listdir('Photos')
+    # disk_file_path = 'Photos_VK'
+    # uploader = YaUploader(token_yd)
+    # result = uploader.upload(disk_file_path, filelist)
