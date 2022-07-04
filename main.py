3981d06e886154d59a300ccf2e69e984bd5fc98e
Liaksej <tarusov.liaksej@gmail.com>
2022-07-04 19:27:38 +0200

    Make a new filename 1.1
    In this version the program is capable to save name like a counting of likes and date of creating.

--- main.py
+++ main.py
@@ -1,73 +1,62 @@
 import requests
 import os
-from pprint import pprint
+from datetime import datetime
 
 
-class VkToYaDsk:
+class VkDownload:
 
     def __init__(self, token: str):
         self.token = token
 
-    def download_photo(self, file_path_to_save, owner_id: int):
+    def download_photo(self, file_path_to_local_save: str, owner_id: int):
         url = 'https://api.vk.com/method/photos.get'
-        # headers = self.get_headers()
         params = {'owner_id': owner_id, 'album_id': 'profile', 'extended': 1, 'access_token': self.token, 'v': '5.131'}
         response = requests.get(url=url, params=params)
         photoalbum = response.json()
-        if not os.path.exists(file_path_to_save):
-            os.mkdir(file_path_to_save)
-            print('Successfully created directory', file_path_to_save)
-        album_best_resolution = []
-        data_for_json = []
-        content_for_json_item = {}
+        if not os.path.exists(file_path_to_local_save):
+            os.mkdir(file_path_to_local_save)
+            print('Успешно создана папка', file_path_to_local_save)
+        control_naming_pattern = {}
         for y in photoalbum['response']['items']:
-            maximum_height_photo = y['sizes'][-1]
-            album_best_resolution.append(maximum_height_photo['url'])
-            content_for_json_item['file_name'] = (f'{y["likes"]["count"]}.jpg')
-            content_for_json_item['size'] = (f'{y["sizes"][-1]["height"]}x{y["sizes"][-1]["width"]}.jpg')
-            data_for_json.append(content_for_json_item)
+            file_name = f'{y["likes"]["count"]}.jpg'
+            if file_name in control_naming_pattern:
+                control_naming_pattern[file_name] += 1
+            else:
+                control_naming_pattern[file_name] = 1
+        data_for_json = []
+        for y in photoalbum['response']['items']:
+            content_for_json_item = {}
             reqs = requests.get(y['sizes'][-1]['url'])
-            with open(file_path_to_save + '/' + f'{y["likes"]["count"]}.jpg', 'wb') as code:
-                code.write(reqs.content)
-        with open(file_path_to_save + '/' + f'photo_data.json', 'w') as file:
+            if f'{y["likes"]["count"]}.jpg' in control_naming_pattern and\
+                    control_naming_pattern[f'{y["likes"]["count"]}.jpg'] > 1:
+                with open(file_path_to_local_save + '/' +
+                          (f'{y["likes"]["count"]}_'
+                           f'{datetime.utcfromtimestamp(int(y["date"])).strftime("%d.%m.%Y")}.jpg'),
+                          'wb') as code:
+                    code.write(reqs.content)
+                content_for_json_item['file_name'] = \
+                    f'{y["likes"]["count"]}_{datetime.utcfromtimestamp(int(y["date"])).strftime("%d.%m.%Y")}.jpg'
+                content_for_json_item['size'] = f'{y["sizes"][-1]["height"]}x{y["sizes"][-1]["width"]}'
+                data_for_json.append(content_for_json_item)
+            else:
+                with open(file_path_to_local_save + '/' + f'{y["likes"]["count"]}.jpg', 'wb') as code:
+                    code.write(reqs.content)
+                content_for_json_item['file_name'] = f'{y["likes"]["count"]}.jpg'
+                content_for_json_item['size'] = f'{y["sizes"][-1]["height"]}x{y["sizes"][-1]["width"]}'
+                data_for_json.append(content_for_json_item)
+        with open(file_path_to_local_save + '/' + 'photo_data.json', 'w') as file:
             file.write(f'{data_for_json}')
 
-        # counter_of_words_in_phrase = {}
-        # for element in data_for_json:
-        #     m = element['file_name']
-        #     if m in counter_of_words_in_phrase:
-        #         counter_of_words_in_phrase[m] += 1
-        #     else:
-        #         counter_of_words_in_phrase[m] = 1
-        # for x counter_of_words_in_phrase.values():
-        #     if x > 1:
-        #         del counter_of_words_in_phrase[x]
-        # for x in data_for_json:
-        #     if x['file_name'] in counter_of_words_in_phrase.keys():
-        #     x['file_name'] = (f'{y["likes"]["count"]} {}.jpg') + return album_best_resolution, data_for_json
-        # pprint(photoalbum)
 
-    # def save_photo(self, file_path_to_save):
-    #     if not os.path.exists(file_path_to_save):
-    #         os.mkdir(file_path_to_save)
-    #         print('Successfully created directory', file_path_to_save)
-    #     listik = self.download_photo(owner_id)
-    #     for y in enumerate(listik[0]):
-    #         reqs = requests.get(y)
-    #         with open(file_path_to_save + '/' + f'{filestr(x)}.jpg', 'wb') as code:
-    #             code.write(reqs.content)
-        # return 'Done'
-
-
-class YaUploader(VkToYaDsk):
+class YaDiscUpload(VkDownload):
 
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token)
+            'Authorization': f'OAuth {self.token}'
         }
 
-    def upload(self, disk_file_path: str, filelist):
+    def upload(self, disk_file_path: str, filelist: list):
         url_for_dir = 'https://cloud-api.yandex.net/v1/disk/resources'
         headers = self.get_headers()
         params_for_dir = {'path': disk_file_path}
@@ -86,16 +75,16 @@ class YaUploader(VkToYaDsk):
         else:
             print('Ошибка соединения. Попробуйте еще раз или обратитесь к разработчнику.')
 
+
 if __name__ == '__main__':
-    token_vk = 'vk1.a.kYn1IfCsd4Di9_f7a0vwj6fhdbLvVa9cFhg7W_EKaczjrTMK8PbY5NJqK4mVbCuo382b5-EEI1moN4Su2sJvyeg1GkV2zGPAP0x9p-PmiXH9jI28O51q4HVpqczwe52WLkikcVdE6-c1zUim5gPJU9okS6hw3DizzYeZgeNTxNefCMQ8NVXJY0AT8_7FU2dx'
-    owner_id = 7451160
+    token_vk = str(input('Введите токен VK: '))
+    owner_id = str(input('Введите ваш id в VK: '))
     file_path_to_save = 'Photos'
-    downloader = VkToYaDsk(token_vk)
-    # downloader.save_photo(file_path_to_save)
+    downloader = VkDownload(token_vk)
     downloader.download_photo(file_path_to_save, owner_id)
 
-    # token_yd = 'AQAAAAAirg3lAADLWyKCRKHJ4kpflbLSVeaV5Go'
-    # filelist = os.listdir('Photos')
-    # disk_file_path = 'Photos_VK'
-    # uploader = YaUploader(token_yd)
-    # result = uploader.upload(disk_file_path, filelist)
+    token_yd = str(input('Введите токен YandexDisk: '))
+    filelist = os.listdir('Photos')
+    disk_file_path = 'Photos_VK'
+    uploader = YaDiscUpload(token_yd)
+    uploader.upload(disk_file_path, filelist)
